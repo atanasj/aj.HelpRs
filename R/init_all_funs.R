696f5dc121b3b22294aaef48ae81e0d2c285bae8
atanas <atanas.atanas@gmail.com>
2022-09-20 21:41:50 +1000

    add corstars

--- R/init_all_funs.R
+++ R/init_all_funs.R
@@ -228,3 +228,55 @@ workspace_size <-
     class(ws) <- "object_size"
     ws
   }
+
+
+##' @title Create correlation tables for printing
+##' @description This is a function to create correlation tables. The expecation
+##'   is that this will be passed meting linke `kable`.
+##' @importFrom psych corr.test
+##' @param data your data
+##' @param method correlation methods taken from "psych::corr.test()", defaults to "pearson". # nolint
+##' @param removeTriangle whic triangle do you want emptied? Defaults to "upper". # nolint
+##' @param result how do you want your starts wrapped? Defaults to "none".
+##' @export
+corstars <-
+  function(data,
+           method = c("pearson", "spearman", "kendall"),
+           removeTriangle = c("upper", "lower"),
+           result = c("none", "html")) {
+    ## require(psych)
+    data <- as.matrix(data)
+    correlation_matrix <- corr.test(data, method = method[1])
+    R <- correlation_matrix$r # Matrix of correlation coeficients
+    p <- correlation_matrix$p # Matrix of p-value
+    ## Define notions for significance levels; spacing is important.
+    if (result[1] == "none") {
+      mystars <- ifelse(p < .001, "***", ifelse(p < .01, "** ", ifelse(p < .05, "*  ", "   "))) # nolint
+    } else if (result[1] == "html") {
+      mystars <-
+        ifelse(p < .001, "<sup>***</sup>", ifelse(p < .01, "<sup>** </sup>", ifelse(p < .05, "<sup>*  </sup>", "<sup>   </sup>"))) # nolint
+    }
+    ## trunctuate the correlation matrix to two decimal
+    R <- format(round(cbind(rep(-1.11, ncol(data)), R), 2))[, -1]
+    ## build a new matrix that includes the correlations with their apropriate
+    ## stars
+    Rnew <- matrix(paste(R, mystars, sep = ""), ncol = ncol(data))
+    diag(Rnew) <- paste(diag(R), " ", sep = "")
+    rownames(Rnew) <- colnames(data)
+    colnames(Rnew) <- paste(colnames(data), "", sep = "")
+    ## remove upper triangle of correlation matrix
+    if (removeTriangle[1] == "upper") {
+      Rnew <- as.matrix(Rnew)
+      Rnew[upper.tri(Rnew, diag = TRUE)] <- ""
+      Rnew <- as.data.frame(Rnew)
+    } else
+    ## remove lower triangle of correlation matrix
+    if (removeTriangle[1] == "lower") {
+      Rnew <- as.matrix(Rnew)
+      Rnew[lower.tri(Rnew, diag = TRUE)] <- ""
+      Rnew <- as.data.frame(Rnew)
+    }
+    ## remove last column and return the correlation matrix
+    Rnew <- cbind(Rnew[1:length(Rnew) - 1])
+    return(Rnew)
+  }
